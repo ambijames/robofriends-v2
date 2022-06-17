087b90e5abd74736f4b60c2a2e6544d1ad2a4e3c
Emmanuel Ambi James <emmaabison@gmail.com>
2022-06-17 01:32:59 +0100

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Card = ({name,email, id, username}) => {
    return (
       <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-s'>
-         <img alt="robots" src={`https://robohash.org/${id}/set_set5?150x150`} />
+         <img alt="robots" src={`https://robohash.org/${id}/set=set5?150x150`} />
          <div>
             <h2>{name}</h2>
             <p>{email}</p>
